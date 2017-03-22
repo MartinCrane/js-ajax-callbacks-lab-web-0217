fd1f02a14f989a88a85b265ffc2c84d4edfbba91
MartinCrane <martin.crane@gmail.com>
2017-03-22 16:30:52 -0400

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,34 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+$( "#searchTermsClick" ).click(searchRepositories);
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+    let term = document.getElementById('searchTerms').value
+    $.get(`https://api.github.com/search/repositories?q=${term}`, function(response) {
+      let templateGet = document.getElementById("results-template").innerHTML
+      let template = Handlebars.compile(templateGet)
+      let repoList = template(response)
+      document.getElementById("results").innerHTML = repoList
+      $("#sentences").html(response);
+   }).fail(function (error) {
+     displayError();
+   });
+ }
+
+ function showCommits(event, data) {
+    let url = data.id.slice(0,-6)
+    $.get(url, function(response) {
+     let templateGet = document.getElementById("details-template").innerHTML
+     let template = Handlebars.compile(templateGet)
+     let repoList = template(response)
+     document.getElementById("details").innerHTML = repoList;
+ }).fail(function (error) {
+   displayError();
+ })
+ }
